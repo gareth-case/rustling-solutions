658713b83abd2c62058eabad3090a05a7e47dd44
gareth-case <garethcase73@gmail.com>
2023-06-26 14:55:13 +0100

    Solutions for clippy, macros, smart pointers

---
+++ exercises/clippy/target/debug/deps/clippy3-67d389f195468c3d.d
@@ -0,0 +1,8 @@
+C:\Users\garet\Documents\projects\rust\rustlings\exercises\clippy\target\debug\deps\clippy3-67d389f195468c3d.rmeta: clippy3.rs Cargo.toml
+
+C:\Users\garet\Documents\projects\rust\rustlings\exercises\clippy\target\debug\deps\clippy3-67d389f195468c3d.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
